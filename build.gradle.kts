64295f91570e0261c9d10fa4c8627961ef1b0249
rajk2007 <2.17292652e+08+rajk2007@users.noreply.github.com>
2026-05-26 05:53:30 +0000

    fix: resolve repository conflict by centralizing config in settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
